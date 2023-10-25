a2176c07875bb1c663f688fc5c7befd4552e482c
chikiryava <alex.man20151@gmail.com>
2023-10-25 13:55:06 +0500

    commit

---
+++ app/src/main/java/com/example/practic221/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.practic221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Header
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.nio.charset.Charset
+
+private val key:String = "e8tMZlKPTYXXHOIlj3lQxreacFnTte6O"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getCurrency("eur","rub",10)
+    }
+    private fun getCurrency(from:String,to:String,amount:Int) {
+        val url = "https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = object: StringRequest(Request.Method.GET, url,
+            Response.Listener{ response ->
+
+                Log.d("responseResult", "Response is: ${JSONObject(response).getString("result")}")
+            },
+            Response.ErrorListener {
+                Log.d("responseResult","error")
+            })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["apikey"] = key
+                return headers
+            }
+        }
+        queue.add(stringRequest)
+    }
+
+}
