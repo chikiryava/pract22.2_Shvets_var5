7b720268290e0abd608830616d786f4d25537f7b
chikiryava <chikiryava@gmail.com>
2023-10-26 23:20:18 +0500

    binding

--- app/src/main/java/com/example/practic221/MainActivity.kt
+++ app/src/main/java/com/example/practic221/MainActivity.kt
@@ -9,14 +9,18 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.practic221.databinding.ActivityMainBinding
 import org.json.JSONObject
 import java.nio.charset.Charset
 
 private val key:String = "e8tMZlKPTYXXHOIlj3lQxreacFnTte6O"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         getCurrency("eur","rub",10)
     }
     private fun getCurrency(from:String,to:String,amount:Int) {
